67b7421faf9e3a8d7f99d562313406de1927aaca
Frankey-webtech <frankeydaboss@gmail.com>
2026-08-29 23:39:29 +0100

    Add files via upload

--- api.js
+++ api.js
@@ -1,3 +1,18 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./service-worker.js")
+            .then(() => {
+                console.log("Service Worker registered successfully.");
+            })
+            .catch(error => {
+                console.error(
+                    "Service Worker registration failed:",
+                    error
+                );
+            });
+    });
+}
+
 if (!window.Parse) {
   console.error("Parse SDK not loaded!");
 } else {
